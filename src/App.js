836141f6616eacea25545bc9d29c52e471d11f9f
Aleksa Bubanja <aleksa3b@gmail.com>
2022-05-05 20:47:11 +0200

    alan ai setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,21 @@
+import alanBtn from "@alan-ai/alan-sdk-web";
+import { useEffect } from "react";
+
 function App() {
+  const key =
+    "6021b8d3aee085781766709cc7c80efe2e956eca572e1d8b807a3e2338fdd0dc/stage";
+
+  useEffect(() => {
+    alanBtn({
+      key,
+      onCommand: ({ command }) => {
+        if (command === "testCommand") {
+          alert("code");
+        }
+      },
+    });
+  }, []);
+
   return <></>;
 }
 
