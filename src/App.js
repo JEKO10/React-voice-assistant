12b811588b2f7cc62ac305974de90c4ff6ea1ca8
Aleksa Bubanja <aleksa3b@gmail.com>
2022-05-06 23:23:58 +0200

    getting voice value from alan to react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import { useState } from "react";
 import useAlan from "./useAlan";
 
 function App() {
-  const [weatherData, setWeatherData] = useState([]);
-
-  const fetchWeather = async (location) => {
-    const response = await fetch(
-      `https://newsapi.org/v2/top-headlines?sources=${location}&apiKey=b19cbd60c8e94a4d990a4ab78de5e958`
-    );
-    const data = await response.json();
-    setWeatherData(data);
+  const fetchTranslate = async (location) => {
+    //fetch
   };
 
-  useAlan({ fetchWeather });
+  useAlan({ fetchTranslate });
 
   return <></>;
 }
--- src/useAlan.js
+++ src/useAlan.js
@@ -1,7 +1,9 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 
-export default function useAlan({ fetchWeather }) {
+export default function useAlan({ fetchTranslate }) {
+  const [translate, setTranslate] = useState("");
+
   const key =
     "6021b8d3aee085781766709cc7c80efe2e956eca572e1d8b807a3e2338fdd0dc/stage";
 
@@ -13,10 +15,13 @@ export default function useAlan({ fetchWeather }) {
         key,
         onCommand: async (data) => {
           if (data.location) console.log(data.location);
+          setTranslate(data.location);
         },
       });
     }
-  }, []);
+
+    console.log(translate);
+  }, [translate]);
 
   return null;
 }
