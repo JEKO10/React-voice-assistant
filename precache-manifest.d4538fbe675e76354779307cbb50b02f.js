9e48bd8916b178823e7be55b490365b3086ed7a7
Aleksa Bubanja <aleksa3b@gmail.com>
2022-05-08 23:54:35 +0200

    Updates

---
+++ precache-manifest.d4538fbe675e76354779307cbb50b02f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "47ae3afe6028683beae7",
+    "url": "/Voice-assistant/static/css/main.fb9c608f.chunk.css"
+  },
+  {
+    "revision": "47ae3afe6028683beae7",
+    "url": "/Voice-assistant/static/js/main.47ae3afe.chunk.js"
+  },
+  {
+    "revision": "4422e644433f4a61be16",
+    "url": "/Voice-assistant/static/js/1.4422e644.chunk.js"
+  },
+  {
+    "revision": "a5fac81892807923886e",
+    "url": "/Voice-assistant/static/js/2.a5fac818.chunk.js"
+  },
+  {
+    "revision": "422e7d419bfcf85b2bb6",
+    "url": "/Voice-assistant/static/js/runtime~main.422e7d41.js"
+  },
+  {
+    "revision": "22f5c91d482cffd0eea3ae2aa633038d",
+    "url": "/Voice-assistant/index.html"
+  }
+];
