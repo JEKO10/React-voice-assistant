178779bcdd64e54cbaee7f7a6a1993a460e88cb5
Aleksa Bubanja <aleksa3b@gmail.com>
2022-05-09 23:29:52 +0200

    Updates

--- precache-manifest.f8098ac29cc5ba19d9973ac7372404c2.js
+++ precache-manifest.f8098ac29cc5ba19d9973ac7372404c2.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "47ae3afe6028683beae7",
+    "revision": "f5516252dc124325be65",
     "url": "/Voice-assistant/static/css/main.fb9c608f.chunk.css"
   },
   {
-    "revision": "47ae3afe6028683beae7",
-    "url": "/Voice-assistant/static/js/main.47ae3afe.chunk.js"
+    "revision": "f5516252dc124325be65",
+    "url": "/Voice-assistant/static/js/main.f5516252.chunk.js"
   },
   {
     "revision": "4422e644433f4a61be16",
     "url": "/Voice-assistant/static/js/1.4422e644.chunk.js"
   },
   {
-    "revision": "a5fac81892807923886e",
-    "url": "/Voice-assistant/static/js/2.a5fac818.chunk.js"
+    "revision": "5f93d07dbd72f7bd6ed0",
+    "url": "/Voice-assistant/static/js/2.5f93d07d.chunk.js"
   },
   {
     "revision": "422e7d419bfcf85b2bb6",
     "url": "/Voice-assistant/static/js/runtime~main.422e7d41.js"
   },
   {
-    "revision": "22f5c91d482cffd0eea3ae2aa633038d",
+    "revision": "abceb31522373a73a407be4a5294419a",
     "url": "/Voice-assistant/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Voice-assistant/precache-manifest.d4538fbe675e76354779307cbb50b02f.js"
+  "/Voice-assistant/precache-manifest.f8098ac29cc5ba19d9973ac7372404c2.js"
 );
 
 workbox.clientsClaim();
